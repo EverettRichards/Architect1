0ff244f70ad2c4aff24d103d9f8ccab6e5ea3b84
Andrew Huang <ahuang@sdccd.edu>
2023-09-06 22:43:45 -0700

    Added example classes

---
+++ Common/src/main/java/edu/sdccd/cisc191/common/entities/StockList.java
@@ -0,0 +1,6 @@
+package edu.sdccd.cisc191.common.entities;
+
+public class StockList {
+    private Stock[] stocks;
+    private User owner;
+}
---
+++ Common/src/main/java/edu/sdccd/cisc191/common/entities/User.java
@@ -0,0 +1,5 @@
+package edu.sdccd.cisc191.common.entities;
+
+public class User {
+    private String name;
+}
