705cb8eb5a4dac1088fc9e19be6968b389648d30
EverettRichards <ehrichards9@gmail.com>
2023-09-14 15:50:56 -0700

    created index.html

--- Server/src/main/java/edu/sdccd/cisc191/server/repositories/ParseFinancialCSV.java
+++ Server/src/main/java/edu/sdccd/cisc191/server/repositories/ParseFinancialCSV.java
@@ -10,28 +10,30 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class ParseFinancialCSV {
-    public static void main(String[] args){
-        Stock apple = new Stock("AAPL","Apple","Makes iPhones and overpriced headphones","Tech",296.50,2.39);
+    public static void main(String[] args) throws IOException, CsvValidationException{
+        /*Stock apple = new Stock("AAPL","Apple","Makes iPhones and overpriced headphones","Tech",296.50,2.39);
         Stock amazon = new Stock("AMZN","Amazon","A worldwide distribution company","IT",3096.30,5.43);
         Stock microsoft = new Stock("MSFT","Miscorosft","A major tech company","IT",495.29,0.50);
         Stock[] myStockList = {apple,amazon,microsoft};
         StockList techStocks = new StockList(myStockList);
         String totalOutput = techStocks.getAllStockInfo();
         System.out.println(techStocks.getTickers());
-        System.out.println(totalOutput);
+        System.out.println(totalOutput);*/
+        ParseCSV("Server/src/main/resources/SP500in2018.csv");
     }
-
-    public ParseFinancialCSV(String fileAddress) throws IOException, CsvValidationException {
+    public static void ParseCSV(String fileAddress) throws IOException, CsvValidationException {
         try (FileReader fr = new FileReader(fileAddress,StandardCharsets.UTF_8);
              CSVReader reader = new CSVReader(fr)) {
-
+            System.out.println("Check1");
             String[] nextLine;
             while ((nextLine = reader.readNext()) != null) {
                 for (String e : nextLine) {
                     System.out.println(e);
                 }
             }
-
         }
+    }
+    public ParseFinancialCSV(){
+
     }
 }
