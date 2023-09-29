6ab7aa5480f954a74259108f2b733be308e9dc8a
Sean Standen <standen.sean@gmail.com>
2023-09-29 15:31:57 -0700

    Have the data coming from the API but having trouble converting from JSON string to Java UIStock objects list or array.

--- Client/src/main/java/edu/sdccd/cisc191/client/controllers/ClientController.java
+++ Client/src/main/java/edu/sdccd/cisc191/client/controllers/ClientController.java
@@ -1,35 +1,27 @@
 package edu.sdccd.cisc191.client.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import edu.sdccd.cisc191.client.models.ClientFetcher;
 import edu.sdccd.cisc191.client.models.UIStock;
-import org.springframework.asm.TypeReference;
 import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
-
-import java.net.URL;
-//import java.nio.charset.MalformedInputException;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-
-import java.io.InputStreamReader;
 import java.util.List;
 
 @Controller
 public class ClientController {
 
-    @GetMapping("/")
-    public String index() throws JsonProcessingException {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String stocksString = ClientFetcher.getRequest("http://localhost:8080/api/stocks");
+    RestTemplate restTemplate = new RestTemplate();
+    String resourceURL = "http://localhost:8080/api";
 
-        System.out.println(stocksString);
+    @GetMapping("/")
+    public String index() {
+
+        List<UIStock> stocks = restTemplate.getForObject(
+                resourceURL + "/stocks",
+                List.class
+        );
+
+        System.out.println(stocks);
         return "index";
     }
 }
