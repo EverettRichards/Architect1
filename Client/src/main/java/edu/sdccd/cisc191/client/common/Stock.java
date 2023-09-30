dd628744aa4580faf722f59dbccd11bd1096208e
leslieboyd3 <lboyd@student.sdccd.edu>
2023-09-30 13:49:58 -0700

    Merge pull request #24 from CISC191/stockcandletest
    Added unit tests + more robust constructors for StockCandle.java
    
    Added Javadoc comments to the Stock.java class

--- Client/src/main/java/edu/sdccd/cisc191/client/common/Stock.java
+++ Client/src/main/java/edu/sdccd/cisc191/client/common/Stock.java
@@ -9,23 +9,31 @@ import edu.sdccd.cisc191.common.entities.Requests;
 import java.net.MalformedURLException;
 //import com.fasterxml.jackson.annotation.JsonProperty;
 
+/**
+ * Stock class that is used to create objects displaying the current
+ * information about stocks being traded on the Exchange
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Stock {
     //@JsonProperty("id")
     private Long id;
 
-    private String ticker;
-    private String name;
-    private String description;
-    private double sharePrice;
-    private double dividendYield;
-    private String stockSector;
+
+    private String ticker;          //the stock ticker or symbol listed on the exchange
+    private String name;            //the company name that issued the stock
+    private String description;     //description of the company issuing the stock
+    private double sharePrice;      //the stock price of one share
+    private double dividendYield;   //the yield of any dividends the company issues
+    private String stockSector;     //the sector or market that the company operates in
 
     public static long lastId = 0L;
 
+    /**
+     * Default constructor for the Stock class
+     */
     public Stock () {
-
     }
+
     public Stock(long id, String ticker, String newName, String description,
                  String sector, double price, double dividend){
         id = ++lastId;
@@ -69,50 +77,98 @@ public class Stock {
         Update();
     }
 
+    /**
+     * Gets the stock ticker symbol
+     * @return ticker the company symbol on the exchange
+     */
     public String getTicker() {
         return ticker;
     }
 
+    /**
+     * Sets the company stock ticker symbol
+     * @param ticker the symbol listed on the exchange
+     */
     public void setTicker(String ticker) {
         this.ticker = ticker;
     }
 
+    /**
+     * Gets the company name of the stock
+     * @return name the company name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the company name for the stock being created or updated
+     * @param name company name of the stock
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * The company description to provide basic info on stocks
+     * @return description the company description
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Sets the information that describes what the company does
+     * @param description the company description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
 
+    /**
+     * Gets the share price of the stock
+     * @return sharePrice the cost of a share of stock
+     */
     public double getPrice() {
         return sharePrice;
     }
 
+    /**
+     * Sets the share price of the stock
+     * @param price the price of a stock share
+     */
     public void setPrice(double price) {
         this.sharePrice = price;
     }
 
+    /**
+     * Gets the value of any dividends that the company issues
+     * @return dividendYield the amount of the dividend
+     */
     public double getDividend() {
         return dividendYield;
     }
 
+    /**
+     * Sets the value of the dividend yield that is being calculated
+     * @param dividend the dividend yield for this stock
+     */
     public void setDividend(double dividend) {
         this.dividendYield = dividend;
     }
 
+    /**
+     * The sector that the company does business in
+     * @return stockSector the business sector
+     */
     public String getSector() {
         return stockSector;
     }
 
+    /**
+     * Sets the business sector that the company operates within
+     * @param name the business sector name
+     */
     public void setSector(String name) {
         this.stockSector = name;
     }
@@ -121,6 +177,10 @@ public class Stock {
         return this.id;
     }
 
+    /**
+     * The custom toString for writing the stock info
+     * @return String formatted string with stock info to be displayed
+     */
     public String toString() {
         return String.format("[%s] %s. Sect: %s. Desc: %s. $%.2f/share.",
                 ticker,name,stockSector,description,sharePrice);
