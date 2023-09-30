fab6c8516f097dc2824ce509376f8ef09c82c8ed
mingzezh <mingzezh@users.noreply.github.com>
2023-09-30 12:10:14 -0700

    fixed remove button redirects

--- Client/src/main/resources/static/js/stockManager.js
+++ Client/src/main/resources/static/js/stockManager.js
@@ -1,8 +1,9 @@
 const baseURL = "/api/stocks/"
-async function remove(event, id) {
+async function remove(event, id, redirect) {
     event.preventDefault();
     let response = await fetch(baseURL + id, {
         method: "DELETE",
     });
-    location.reload();
+    //location.reload();
+    window.location = redirect;
 }
