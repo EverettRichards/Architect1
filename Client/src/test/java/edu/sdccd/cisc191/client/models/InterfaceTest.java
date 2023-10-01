26774f69faf1fc803ff805fec526c6efd9a281d0
EverettRichards <ehrichards9@gmail.com>
2023-09-30 23:18:00 -0700

    Added support for varied-duration stock candle indexes

---
+++ Client/src/test/java/edu/sdccd/cisc191/client/models/InterfaceTest.java
@@ -0,0 +1,16 @@
+package edu.sdccd.cisc191.client.models;
+
+import edu.sdccd.cisc191.client.controllers.StockController;
+import edu.sdccd.cisc191.common.entities.Stock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class InterfaceTest {
+
+    @Test
+    void testInterface() {
+        // Make sure that the StockController class is indeed an implementation of the DataFetcher interface
+        StockController newObj = new StockController();
+        assertTrue(newObj instanceof DataFetcher);
+    }
+}
