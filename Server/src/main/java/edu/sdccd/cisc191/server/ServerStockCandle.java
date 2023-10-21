36015483d4197790281aa7f5b8093e909c06858c
mingzezh <mingzezh@users.noreply.github.com>
2023-10-21 16:02:12 -0700

    fixed typo

--- Server/src/main/java/edu/sdccd/cisc191/server/ServerStockCandle.java
+++ Server/src/main/java/edu/sdccd/cisc191/server/ServerStockCandle.java
@@ -97,7 +97,7 @@ public class ServerStockCandle extends StockCandle {
     // Call the API to get the latest information. Then, save the new data to the server.
     private void updateFromAPI() throws MalformedURLException, JsonProcessingException, FileNotFoundException {
         String ticker = getTicker();
-        String finnhubResult = FinnhubNetworking.getCandleFromFinnhub(ticker,duration,time1,time2);
+        String finnhubResult = Finnhub.getCandleFromFinnhub(ticker,duration,time1,time2);
         JsonNode root = DataMethods.decodeJson(finnhubResult);
         // ERROR HAPPENS here (below)
         updateFromJsonNode(root,true);
